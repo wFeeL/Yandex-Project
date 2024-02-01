7cf0c0d15b80da8434a1e06da8dba81afe90d8e5
Danya <danny.glazovs@gmail.com>
2024-02-01 20:03:08 +0300

    add game Sota to menu

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import pygame
 import pygame_menu
 import subprocess
 
-
 pygame.init()
 surface = pygame.display.set_mode((600, 400))
 
@@ -14,9 +13,17 @@ def start_the_chess():
         print(f"Error executing game.exe: {e}")
 
 
+def start_the_sota():
+    try:
+        subprocess.run('pygameSota/dist/main.exe', check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"Error executing game.exe: {e}")
+
+
 menu = pygame_menu.Menu('Welcome', 400, 300,
                         theme=pygame_menu.themes.THEME_SOLARIZED)
 menu.add.button('Chess', start_the_chess)
+menu.add.button('Sota', start_the_sota)
 menu.add.button('Quit', pygame_menu.events.EXIT)
 
 menu.mainloop(surface)
